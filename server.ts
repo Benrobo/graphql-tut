4a3b30b656ae5c505cd188fd30e4075e4d249dc4
benrobo <alumonabenaiah71@gmail.com>
2023-07-09 20:25:30 +0100

    updated graphql setup

--- server.ts
+++ server.ts
@@ -1,20 +1,31 @@
 import { ApolloServer } from "@apollo/server";
-import GraphqlSchemaTypeDef from "./schema";
+import GraphqlSchemaTypeDef from "./graphql/schema/index.js";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { userResolver } from "./resolvers";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { userResolver } from "./graphql/resolvers";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-async function startGqlServer() {
-  const resolvers = {
-    ...userResolver,
-  };
-  const server = new ApolloServer({
-    typeDefs: GraphqlSchemaTypeDef,
-    resolvers,
-  });
-  const { url } = await startStandaloneServer(server, {
-    context: async ({ req }) => {},
-  });
-  console.log(`🚀 Server started @ ${url}`);
-}
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs: GraphqlSchemaTypeDef,
+  // resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
 
-export default startGqlServer;
+app.use(
+  "/graphql",
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise((resolve) => resolve(httpServer.listen({ port: 4000 })));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
