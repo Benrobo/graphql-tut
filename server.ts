4f857c7042433d16191c33e5b8d128f3c4987f9b
benrobo <alumonabenaiah71@gmail.com>
2023-07-09 01:27:13 +0100

    added update

--- server.ts
+++ server.ts
@@ -1,10 +1,19 @@
 import { ApolloServer } from "@apollo/server";
 import GraphqlSchemaTypeDef from "./schema";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { userResolver } from "./resolvers";
 
 async function startGqlServer() {
-  const server = new ApolloServer({ typeDefs: GraphqlSchemaTypeDef });
-  const { url } = await startStandaloneServer(server);
+  const resolvers = {
+    ...userResolver,
+  };
+  const server = new ApolloServer({
+    typeDefs: GraphqlSchemaTypeDef,
+    resolvers,
+  });
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => {},
+  });
   console.log(`🚀 Server started @ ${url}`);
 }
 
